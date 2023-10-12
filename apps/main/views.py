6ef265bb9989d854bb9aa73643c3ee087d09e583
AnnSkachkova <anna.vladimirovna2003@mail.ru>
2023-10-12 19:57:04 +0300

    мелкие поправки

--- apps/main/views.py
+++ apps/main/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, HttpResponse
+from django.contrib.auth.views import LoginView
 
-# Create your views here.
+
+class Login(LoginView):
+    template_name = 'login.html'
 
 
 def index(request):
