2017ef3098f2ec5d044d8159015d9b3075fe4819
AnnSkachkova <anna.vladimirovna2003@mail.ru>
2023-10-13 11:49:18 +0300

    при входе дает или обнавляет токен

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'main'
 
 urlpatterns = [
-    path('', views.Login.as_view(), name='login')
+    path('', views.Login.as_view(), name='login'),
+    path('menu/', views.index, name='index')
 ]
--- apps/main/views.py
+++ apps/main/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render, HttpResponse
 from django.contrib.auth.views import LoginView
+from . import models
 
 
 class Login(LoginView):
     template_name = 'login.html'
+    
+    def get_success_url(self) -> str:
+        user = self.request.user
+        token = self.request.POST['user_token']
+        models.Token.objects.update_or_create(user=user, defaults={'token': token})
+        return super(Login, self).get_success_url()
 
 
 def index(request):
