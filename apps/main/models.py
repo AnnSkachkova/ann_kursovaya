3b928902f5218990cc4899b97ef2c9fef157a4e9
AnnSkachkova <anna.vladimirovna2003@mail.ru>
2023-10-13 10:40:51 +0300

    модель токента при регистрации пользователя

--- apps/main/models.py
+++ apps/main/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Token(models.Model):
+    token = models.CharField(max_length=32, verbose_name='Токен')
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='Пользователь')
+    
+    def __str__(self) -> str:
+        return f"Токен {self.token} для пользователя {self.user}"
+    
+    class Meta:
+        verbose_name = 'Токен пользователя'
+        verbose_name_plural = 'Токенты пользователей'
+        
+        
