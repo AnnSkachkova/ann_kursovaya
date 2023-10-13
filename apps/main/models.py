af75a63a9f3ef2a42a91e3b05c7e11ced8797912
AnnSkachkova <anna.vladimirovna2003@mail.ru>
2023-10-13 13:59:04 +0300

    модель продукта создана и давлена в базу

--- apps/main/models.py
+++ apps/main/models.py
@@ -14,3 +14,24 @@ class Token(models.Model):
         verbose_name_plural = 'Токенты пользователей'
         
         
+class BaseDateModel(models.Model):
+    dt_created = models.DateTimeField(verbose_name='Дата и время создания', auto_now_add=True)
+    dt_updated = models.DateTimeField(verbose_name='Дата и время последнего изменения', auto_now=True)
+    to_remove = models.BooleanField(verbose_name='Помечен на удаление', null=False, default=False)
+    
+    class Meta:
+        abstract = True
+
+
+class Product(BaseDateModel):
+    title = models.CharField(max_length=200, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание', null=True, blank=True, default='')
+    price = models.IntegerField(verbose_name='Цена')
+
+    def __str__(self):
+        return '{number:<10}|{title}'.format(number=self.pk, title=self.title)
+    
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+        ordering = ['title']
