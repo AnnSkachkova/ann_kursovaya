26e32b1b15f2fe7c05c156f50e361ce26798b468
AnnSkachkova <anna.vladimirovna2003@mail.ru>
2023-10-13 14:05:40 +0300

    модель продукта добавлена в админку

--- apps/main/admin.py
+++ apps/main/admin.py
@@ -2,8 +2,12 @@ from django.contrib import admin
 from . import models
 
 
-@admin.register(models.Token)
-class TokenAdmin(admin.ModelAdmin):
+vjclass TokenAdmin(admin.ModelAdmin):
     list_display = ['token', 'user']
     list_display_links = ['token', 'user']
     
+    
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description', 'price', 'dt_created', 'dt_updated', 'to_remove']
+    list_display_links = ['title', 'description', 'price']
