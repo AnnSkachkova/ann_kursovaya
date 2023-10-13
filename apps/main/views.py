37a14678e168315c638d6bbb5182163033d1e3bf
AnnSkachkova <anna.vladimirovna2003@mail.ru>
2023-10-13 13:45:03 +0300

    выход из профиля написан

--- apps/main/views.py
+++ apps/main/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic import TemplateView
+from django.urls import reverse_lazy
 from . import models
 
 
@@ -12,6 +12,10 @@ class Login(LoginView):
         token = self.request.POST['user_token']
         models.Token.objects.update_or_create(user=user, defaults={'token': token})
         return super(Login, self).get_success_url()
+
+
+class Logout(LogoutView):
+    next_page = reverse_lazy('login')
 
 
 class HomePage(TemplateView):
--- conf/urls.py
+++ conf/urls.py
@@ -7,6 +7,7 @@ from apps.main import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', views.Login.as_view(), name='login'),
+    path('logout/', views.Logout.as_view(), name='logout'),
     path('', include('apps.main.urls')),
 ]
 
