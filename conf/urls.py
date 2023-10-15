22471c1a249e4aa30c7c8913308aaa068ff713c8
AnnSkachkova <anna.vladimirovna2003@mail.ru>
2023-10-15 17:36:29 +0300

    настройки приложения restapi завершены

--- conf/urls.py
+++ conf/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', views.Login.as_view(), name='login'),
     path('logout/', views.Logout.as_view(), name='logout'),
+    path('api/', include('apps.restapi.urls')),
     path('', include('apps.main.urls')),
 ]
 
